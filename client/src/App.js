13b27272217f56a15c9ee514e2faf5d5d02c8e79
Itserge1 <realitserge@gmail.com>
2023-06-15 11:49:18 -0400

    finale contact solve

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Home from './views/Home/Home';
 import About from './views/About/About';
 import Project from './views/Project/Project';
 
-import Contact from './views/Contact/contact';
+import Contact from './views/Contact/Contact';
 
 import Popup from './components/Popup/Popup';
 
