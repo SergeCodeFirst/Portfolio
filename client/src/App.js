26d6ef8236ef5051d86755a163127626c035db01
SergeH <91694863+Itserge1@users.noreply.github.com>
2023-06-22 16:05:07 -0400

    add background circles (#4)

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,12 @@ function App() {
   }
   return (
     <div className= "App">
+      <div className="bg-circles">
+        <div className="circle-1"></div>
+        <div className="circle-2"></div>
+        <div className="circle-3"></div>
+        <div className="circle-4"></div>
+      </div>
       <BrowserRouter>
       <Switch>
         <Route exact path='/'>
